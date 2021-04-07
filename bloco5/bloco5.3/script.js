cb44066168ac89668d8d20a11711d59cbfc31721
marcoswarmling <marcoswarmling15@gmail.com>
2021-04-07 19:17:30 -0300

    quando clickado

---
+++ bloco5/bloco5.3/script.js
@@ -0,0 +1,26 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+
+
+
+let clicar = document.querySelector('#btn-add')
+clicar.addEventListener('click',function() {
+
+  console.log(clicar)
+})
+
+
+
+  // Escreva seu código abaixo.
