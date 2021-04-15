64d35969b120eb5fabef03c3f2160b9481053111
marcoswarmling <marcoswarmling15@gmail.com>
2021-04-15 19:53:43 -0300

    6.1 terminar amanha

---
+++ bloco6/bloco6.1/script.js
@@ -0,0 +1,9 @@
+let list = document.getElementsByClassName('list');
+let button = document.getElementById('submit');
+let resul = document.getElementById('resul');
+button.addEventListener('click',function join() {
+    for (let index = 0; index < list.length; index++) {
+   let Stext = list[index].value 
+
+    }
+})
