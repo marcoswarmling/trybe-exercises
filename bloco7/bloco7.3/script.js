ffb7555d17dcd684bae93c2b0f7215702d248a16
marcoswarmling <marcoswarmling15@gmail.com>
2021-04-28 18:46:56 -0300

    testes unitarios

---
+++ bloco7/bloco7.3/script.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+let remove = myRemove(1, 6)
+
+console.log(remove)
